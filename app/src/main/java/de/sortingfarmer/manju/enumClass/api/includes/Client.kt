f4fcf4691f106166dd03394d1336dae06ab1accc
SortingFarmer <bengemrafdavid@gmail.com>
2025-04-10 16:59:36 +0200

    Refactor: Remove unused enums
    - Removed unused enum classes: `Author`, `Client`, `GrantType`, and `Order`.

--- app/src/main/java/de/sortingfarmer/manju/enumClass/api/includes/Client.kt
+++
@@ -1,9 +0,0 @@
-package de.sortingfarmer.manju.enumClass.api.includes
-
-enum class Client(val value: String) {
-    CREATOR("creator");
-
-    override fun toString(): String {
-        return value
-    }
-}
