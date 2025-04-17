015a9c7be3711e8a6baad1d05895ce542744cae0
SortingFarmer <bengemrafdavid@gmail.com>
2025-04-17 11:24:55 +0200

    Feat: Improve chapter card UI and functionality
    - Improved ChapterCard UI with better layout and click handling.
    - Added click functionality for the card, read marker, group, translator, and comments.
    - Displayed chapter information like title, relative time, comments count, scanlation group, and translator.
    - Used icons for visual cues and MaterialTheme typography for consistent styling.

--- app/src/main/java/de/sortingfarmer/manju/ui/components/Chapter.kt
+++ app/src/main/java/de/sortingfarmer/manju/ui/components/Chapter.kt
@@ -1,126 +1,154 @@
 package de.sortingfarmer.manju.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.sortingfarmer.manju.R
 import de.sortingfarmer.manju.openapi.models.Chapter
-import de.sortingfarmer.manju.openapi.models.StatisticsDetailsComments
+import de.sortingfarmer.manju.openapi.models.GetStatisticsChapterUuid200ResponseStatisticsValue
+import getRelativeTime
 import testChapter
+import testChapterStatistics
+import java.math.BigDecimal
+import java.util.UUID
 
 @Composable
 fun ChapterCard(
     chapter: Chapter,
-    statistics: StatisticsDetailsComments? = null,
+    statistics: GetStatisticsChapterUuid200ResponseStatisticsValue?,
+    modifier: Modifier = Modifier,
     read: Boolean = false,
-    modifier: Modifier = Modifier
+    onClick: () -> Unit,
+    onReadMarkerClick: (id: UUID?) -> Unit,
+    onGroupClick: (id: UUID?) -> Unit = {},
+    onTranslatorClick: (id: UUID?) -> Unit = {},
+    onCommentsClick: (id: BigDecimal?) -> Unit = {},
 ) {
-    val iconsize = 16
+    val iconsize = 30
 
-    Card (
+    Card(
         modifier = modifier
             .padding(16.dp, 8.dp, 16.dp, 8.dp)
-            .fillMaxSize()
+            .clickable(onClick = { onClick() })
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(2.dp)
-        ) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-            ) {
-                if (read) {
-                    IconButton(
-                        onClick = { },
-                        modifier = Modifier.size(iconsize.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.eye_off_outline),
-                            contentDescription = stringResource(R.string.chapter_read),
-                        )
-                    }
-                } else {
-                    IconButton(
-                        onClick = { },
-                        modifier = Modifier.size(iconsize.dp)
-                    ) {
-                        Image(
-                            painter = painterResource(R.drawable.eye_outline),
-                            contentDescription = stringResource(R.string.chapter_unread),
-                        )
-                    }
-                }
-                VerticalDivider()
-                Text(
-                    text = chapter.attributes?.title ?: stringResource(
-                        R.string.vol_ch,
-                        chapter.attributes?.volume ?: 0,
-                        chapter.attributes?.chapter ?: 0
-                    )
+        Column {
+            Row {
+                Image(
+                    painter = painterResource(
+                        if (read) R.drawable.eye_off_outline else R.drawable.eye_outline
+                    ),
+                    contentDescription = stringResource(
+                        if (read) R.string.chapter_unread else R.string.chapter_read
+                    ),
+                    modifier = Modifier
+                        .size(iconsize.dp)
+                        .padding(2.dp)
+                        .clickable(onClick = { onReadMarkerClick(chapter.id) })
                 )
-                VerticalDivider()
-                Row {
-                    Image(
+                Text(
+                    text = chapter.attributes?.title
+                        ?: stringResource(R.string.no_title_available),
+                    modifier = Modifier
+                        .padding(5.dp),
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Row(
+                modifier = Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)
+            ) {
+                Text(
+                    text =
+                        getRelativeTime(chapter.attributes?.readableAt!!).toString(),
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = " • ",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Row(
+                    modifier = Modifier.clickable(onClick = { onCommentsClick(statistics?.comments?.threadId) })
+                ) {
+                    Icon(
                         painter = painterResource(R.drawable.chatbox_outline),
                         contentDescription = stringResource(R.string.comments),
-                        modifier = Modifier.size(iconsize.dp)
+                        modifier = Modifier
+                            .size(15.dp)
+                            .padding(2.dp)
                     )
-                    Text(text = "${statistics?.repliesCount ?: 0.toString()}")
+                    Text(
+                        text = if (statistics?.comments?.repliesCount == null) "0" else statistics.comments.repliesCount.toString(),
+                        style = MaterialTheme.typography.bodySmall
+                    )
                 }
             }
-            HorizontalDivider()
             Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.padding(6.dp, 0.dp, 0.dp, 2.dp)
             ) {
-                Image(
-                    painter = painterResource(R.drawable.people_circle_outline),
-                    contentDescription = stringResource(R.string.group),
-                    modifier = Modifier.size(iconsize.dp)
-                )
-                VerticalDivider()
                 chapter.relationships?.forEach {
                     if (it.type == "scanlation_group") {
-                        Text(text = it.attributes?.name ?: stringResource(R.string.no_name_available))
+                        Text(
+                            text = it.attributes?.name
+                                ?: stringResource(R.string.no_name_available),
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.clickable(onClick = { onGroupClick(it.id) })
+                        )
+                        Text(
+                            text = " • ",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                    if (it.type == "user") {
+                        Text(
+                            text = it.attributes?.username
+                                ?: stringResource(R.string.no_name_available),
+                            style = MaterialTheme.typography.bodySmall,
+                            modifier = Modifier.clickable(onClick = { onTranslatorClick(it.id) })
+                        )
                     }
                 }
-                VerticalDivider()
-                Image(
-                    painter = painterResource(R.drawable.eye_outline),
-                    contentDescription = stringResource(R.string.views),
-                    modifier = Modifier.size(iconsize.dp)
-                )
-                Text(text = "N/A")
             }
         }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ChapterCardReadPreview() {
-    ChapterCard(testChapter, read = true)
+    ChapterCard(
+        testChapter,
+        read = true,
+        statistics = testChapterStatistics,
+        onClick = {},
+        onReadMarkerClick = {},
+        onGroupClick = {},
+        onTranslatorClick = {},
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ChapterCardUnreadPreview() {
-    ChapterCard(testChapter, read = false)
+    ChapterCard(
+        testChapter,
+        read = false,
+        statistics = testChapterStatistics,
+        onClick = {},
+        onReadMarkerClick = {},
+        onGroupClick = {},
+        onTranslatorClick = {},
+    )
 }
