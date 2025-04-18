0ccea5f716473d9c132f712a1fd8a818c31cc6b5
SortingFarmer <bengemrafdavid@gmail.com>
2025-04-18 12:36:53 +0200

    Feat: Display manga statistics on manga details screen
    - Added statistics API call to fetch and display manga statistics like follows and rating on the manga details screen.

--- app/src/main/java/de/sortingfarmer/manju/ui/screen/sub/MangaScreen.kt
+++ app/src/main/java/de/sortingfarmer/manju/ui/screen/sub/MangaScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.runtime.setValue
 import androidx.navigation.NavHostController
 import de.sortingfarmer.manju.RetrofitClient
 import de.sortingfarmer.manju.openapi.apis.MangaApi
+import de.sortingfarmer.manju.openapi.apis.StatisticsApi
+import de.sortingfarmer.manju.openapi.models.GetStatisticsMangaUuid200Response
 import de.sortingfarmer.manju.openapi.models.MangaResponse
 import de.sortingfarmer.manju.ui.components.MangaTextCard
 import java.util.UUID
@@ -16,6 +18,7 @@ import java.util.UUID
 @Composable
 fun MangaScreen(navController: NavHostController, id: String) {
     var manga by remember { mutableStateOf<MangaResponse?>(null) }
+    var statistics by remember { mutableStateOf<GetStatisticsMangaUuid200Response?>(null) }
 
     LaunchedEffect(key1 = Unit) {
         manga = RetrofitClient.instance.create(MangaApi::class.java)
@@ -23,7 +26,15 @@ fun MangaScreen(navController: NavHostController, id: String) {
                 id = UUID.fromString(id),
                 includes = listOf("cover_art", "tags")
             ).body()
+
+        statistics = RetrofitClient.instance.create(StatisticsApi::class.java)
+            .getStatisticsMangaUuid(
+                uuid = UUID.fromString(id)
+            ).body()
     }
 
-    manga?.let { MangaTextCard(manga = it.data!!) {} }
+    manga?.let { MangaTextCard(
+        manga = it.data!!,
+        statistics = statistics?.statistics?.get(id)
+    ) {} }
 }
