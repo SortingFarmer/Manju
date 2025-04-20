602d4c83bd230a91d6b30d3aeffd0313998c4105
SortingFarmer <bengemrafdavid@gmail.com>
2025-04-20 19:33:14 +0200

    Feat: Enhance chapter card with translator, group, and comment info
    - Added a dropdown menu to the chapter card to display translator and scanlation group information.
    - Integrated API calls to fetch chapter statistics, including comment counts.
    - Displayed relative time of chapter release.
    - Added clickable functionality to view comments.
    - Reorganized information display for better readability.

--- app/src/main/java/de/sortingfarmer/manju/ui/components/Chapter.kt
+++ app/src/main/java/de/sortingfarmer/manju/ui/components/Chapter.kt
@@ -9,11 +9,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -22,21 +29,21 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.sortingfarmer.manju.R
+import de.sortingfarmer.manju.RetrofitClient
+import de.sortingfarmer.manju.openapi.apis.StatisticsApi
 import de.sortingfarmer.manju.openapi.models.Chapter
-import de.sortingfarmer.manju.openapi.models.GetStatisticsChapterUuid200ResponseStatisticsValue
+import de.sortingfarmer.manju.openapi.models.GetStatisticsChapterUuid200Response
 import de.sortingfarmer.manju.ui.theme.ManjuTheme
 import de.sortingfarmer.manju.ui.theme.ManjuThemeExtended
 import formatNumber
 import getRelativeTime
 import testChapter
-import testChapterStatistics
 import java.math.BigDecimal
 import java.util.UUID
 
 @Composable
 fun ChapterCard(
     chapter: Chapter,
-    statistics: GetStatisticsChapterUuid200ResponseStatisticsValue?,
     modifier: Modifier = Modifier,
     read: Boolean = false,
     onClick: () -> Unit,
@@ -46,6 +53,15 @@ fun ChapterCard(
     onCommentsClick: (id: BigDecimal?) -> Unit,
 ) {
     val iconsize = 30
+    var expanded by remember { mutableStateOf(false) }
+    var statistics by remember { mutableStateOf<GetStatisticsChapterUuid200Response?>(null) }
+
+    LaunchedEffect(key1 = Unit) {
+        statistics = RetrofitClient.instance.create(StatisticsApi::class.java)
+            .getStatisticsChapterUuid(
+                uuid = chapter.id!!,
+            ).body()
+    }
 
     Card(
         modifier = modifier
@@ -106,17 +122,70 @@ fun ChapterCard(
             Row(
                 modifier = Modifier.padding(6.dp, 0.dp, 0.dp, 0.dp)
             ) {
-                Text(
-                    text =
-                        getRelativeTime(chapter.attributes?.readableAt!!).toString(),
-                    style = MaterialTheme.typography.bodySmall
-                )
+                Row(
+                    modifier = Modifier.clickable(onClick = { expanded = !expanded })
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.language_outline),
+                        contentDescription = stringResource(R.string.translators),
+                        modifier = Modifier
+                            .size(15.dp)
+                            .padding(2.dp)
+                    )
+                    Text(
+                        text = stringResource(R.string.translators),
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+                DropdownMenu(
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    chapter.relationships?.forEach {
+                        if (it.type == "scanlation_group") {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(
+                                        text = it.attributes?.name
+                                            ?: stringResource(R.string.no_name_available)
+                                    )
+                                },
+                                onClick = { onGroupClick(it.id) },
+                                leadingIcon = {
+                                    Icon(
+                                        painterResource(R.drawable.people_circle_outline),
+                                        contentDescription = stringResource(R.string.group),
+                                        modifier = Modifier.size(30.dp)
+                                    )
+                                }
+                            )
+                        }
+                        if (it.type == "user") {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(
+                                        text = it.attributes?.username
+                                            ?: stringResource(R.string.no_name_available)
+                                    )
+                                },
+                                onClick = { onTranslatorClick(it.id) },
+                                leadingIcon = {
+                                    Icon(
+                                        painterResource(R.drawable.person_circle_outline),
+                                        contentDescription = stringResource(R.string.translator),
+                                        modifier = Modifier.size(30.dp)
+                                    )
+                                }
+                            )
+                        }
+                    }
+                }
                 Text(
                     text = " • ",
                     style = MaterialTheme.typography.bodySmall
                 )
                 Row(
-                    modifier = Modifier.clickable(onClick = { onCommentsClick(statistics?.comments?.threadId) })
+                    modifier = Modifier.clickable(onClick = { onCommentsClick(statistics?.statistics?.get(chapter.id.toString())?.comments?.threadId) })
                 ) {
                     Icon(
                         painter = painterResource(R.drawable.chatbox_outline),
@@ -126,36 +195,19 @@ fun ChapterCard(
                             .padding(2.dp)
                     )
                     Text(
-                        text = formatNumber(statistics?.comments?.repliesCount?.toInt() ?: 0),
+                        text = formatNumber(statistics?.statistics?.get(chapter.id.toString())?.comments?.repliesCount?.toInt() ?: 0),
                         style = MaterialTheme.typography.bodySmall
                     )
                 }
-            }
-            Row(
-                modifier = Modifier.padding(6.dp, 0.dp, 0.dp, 2.dp)
-            ) {
-                chapter.relationships?.forEach {
-                    if (it.type == "scanlation_group") {
-                        Text(
-                            text = it.attributes?.name
-                                ?: stringResource(R.string.no_name_available),
-                            style = MaterialTheme.typography.bodySmall,
-                            modifier = Modifier.clickable(onClick = { onGroupClick(it.id) })
-                        )
-                        Text(
-                            text = " • ",
-                            style = MaterialTheme.typography.bodySmall
-                        )
-                    }
-                    if (it.type == "user") {
-                        Text(
-                            text = it.attributes?.username
-                                ?: stringResource(R.string.no_name_available),
-                            style = MaterialTheme.typography.bodySmall,
-                            modifier = Modifier.clickable(onClick = { onTranslatorClick(it.id) })
-                        )
-                    }
-                }
+                Text(
+                    text = " • ",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text =
+                        getRelativeTime(chapter.attributes?.readableAt!!).toString(),
+                    style = MaterialTheme.typography.bodySmall
+                )
             }
         }
     }
@@ -168,7 +220,6 @@ fun ChapterCardReadPreview() {
         ChapterCard(
             testChapter,
             read = true,
-            statistics = testChapterStatistics,
             onClick = {},
             onReadMarkerClick = {},
             onGroupClick = {},
@@ -185,7 +236,6 @@ fun ChapterCardUnreadPreview() {
         ChapterCard(
             testChapter,
             read = false,
-            statistics = testChapterStatistics,
             onClick = {},
             onReadMarkerClick = {},
             onGroupClick = {},
