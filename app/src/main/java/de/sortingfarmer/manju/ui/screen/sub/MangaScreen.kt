3e5c65d982297bf29ce26191049a57ca5704b606
SortingFarmer <bengemrafdavid@gmail.com>
2025-04-21 22:09:31 +0200

    Feat: Display manga chapters on MangaScreen
    - Added `chapters` state variable to store chapter data.
    - Fetched chapter data from the API using `ChapterApi` in a `LaunchedEffect`.
    - Displayed a list of chapters using `LazyColumn` and `ChapterCard` components.
    - Passed necessary parameters to `ChapterCard` including chapter data, read status, click handlers, and modifier.

--- app/src/main/java/de/sortingfarmer/manju/ui/screen/sub/MangaScreen.kt
+++ app/src/main/java/de/sortingfarmer/manju/ui/screen/sub/MangaScreen.kt
@@ -1,17 +1,23 @@
 package de.sortingfarmer.manju.ui.screen.sub
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import de.sortingfarmer.manju.RetrofitClient
+import de.sortingfarmer.manju.openapi.apis.ChapterApi
 import de.sortingfarmer.manju.openapi.apis.MangaApi
 import de.sortingfarmer.manju.openapi.apis.StatisticsApi
+import de.sortingfarmer.manju.openapi.models.ChapterList
 import de.sortingfarmer.manju.openapi.models.GetStatisticsMangaUuid200Response
 import de.sortingfarmer.manju.openapi.models.MangaResponse
+import de.sortingfarmer.manju.ui.components.ChapterCard
 import de.sortingfarmer.manju.ui.components.MangaTextCard
 import java.util.UUID
 
@@ -19,21 +25,51 @@ import java.util.UUID
 fun MangaScreen(navController: NavHostController, id: String) {
     var manga by remember { mutableStateOf<MangaResponse?>(null) }
     var statistics by remember { mutableStateOf<GetStatisticsMangaUuid200Response?>(null) }
+    var chapters by remember { mutableStateOf<ChapterList?>(null) }
 
     LaunchedEffect(key1 = Unit) {
-        manga = RetrofitClient.instance.create(MangaApi::class.java)
+        manga = RetrofitClient.instance
+            .create(MangaApi::class.java)
             .getMangaId(
                 id = UUID.fromString(id),
                 includes = listOf("cover_art", "tags")
             ).body()
 
-        statistics = RetrofitClient.instance.create(StatisticsApi::class.java)
+        statistics = RetrofitClient.instance
+            .create(StatisticsApi::class.java)
             .getStatisticsMangaUuid(
                 uuid = UUID.fromString(id)
             ).body()
+
+        chapters = RetrofitClient.instance
+            .create(ChapterApi::class.java)
+            .getChapter(
+                manga = UUID.fromString(id),
+                includes = listOf("scanlation_group", "user"),
+                limit = 100
+            ).body()
     }
 
-    manga?.let { MangaTextCard(
-        manga = it.data!!,
-    ) {} }
+    LazyColumn {
+        manga?.let {
+            item {
+                MangaTextCard(
+                    manga = it.data!!,
+                ) {}
+            }
+        }
+        chapters?.data?.forEach {
+            item {
+                ChapterCard(
+                    chapter = it,
+                    read = true,
+                    onClick = {},
+                    onReadMarkerClick = {},
+                    onGroupClick = {},
+                    onUserClick = {},
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
 }
