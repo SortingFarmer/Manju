a4843074a3eef539a50e84186ceb46bc547a6e2c
SortingFarmer <bengemrafdavid@gmail.com>
2025-05-06 12:11:13 +0200

    Feat: Implement grid and list view, add search functionality
    - Added functionality to switch between grid and list views in `AdvancedSearchScreen`.
    - Implemented a search bar that opens in a bottom sheet.
    - Updated dependencies to manage the search functionality.
    - Added an action button for advanced search.
    - Added icons for list, grid, and search.
    - Added the functionality to display the searchbar as a `ModalBottomSheet`.
    - Replaced `Text` with `Icon` for the list/grid view toggle.
    - Created `ListHeader` to manage the header part of the list or grid.
    - Removed unused code.
    - Added `SingleChoiceSegmentedButtonRow` to manage the toggle.

--- app/src/main/java/de/sortingfarmer/manju/ui/screen/AdvancedSearchScreen.kt
+++ app/src/main/java/de/sortingfarmer/manju/ui/screen/AdvancedSearchScreen.kt
@@ -1,26 +1,51 @@
 package de.sortingfarmer.manju.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -30,48 +55,49 @@ import de.sortingfarmer.manju.openapi.apis.MangaApi
 import de.sortingfarmer.manju.openapi.models.MangaList
 import de.sortingfarmer.manju.ui.components.MangaImageCard
 import de.sortingfarmer.manju.ui.components.MangaTextCard
-
+import kotlinx.coroutines.launch
+import retrofit2.Response
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AdvancedSearchScreen(navController: NavController) {
     var manga by remember { mutableStateOf<MangaList?>(null) }
-    var gridState by remember { mutableStateOf(LazyGridState()) }
-    var listState by remember { mutableStateOf(LazyListState()) }
-    var grid by remember { mutableStateOf(true) }
+    val gridState = rememberLazyGridState()
+    val listState = rememberLazyListState()
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    val options = listOf("Cover Grid", "Detailed List")
+    val searchText = remember { mutableStateOf("") }
 
-    LaunchedEffect(key1 = Unit) {
+    LaunchedEffect(Unit) {
         manga = RetrofitClient.instance.create(MangaApi::class.java)
             .getSearchManga(
-                includes = listOf("cover_art", "tags")
+                includes = listOf("cover_art", "tags"),
+                limit = 20
             ).body()
     }
 
     Column {
         if (manga == null) {
-            LinearProgressIndicator(
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         }
-        Row (
-            modifier = Modifier.padding(
-                start = 10.dp,
-                end = 10.dp
-            ),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = stringResource(R.string.grid_enabled, grid))
-            Switch(
-                checked = grid,
-                onCheckedChange = { isChecked -> grid = isChecked },
-            )
-        }
-        if (grid) {
+        if (selectedIndex == 0) {
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
-                modifier = Modifier.padding(start = 6.dp, end = 6.dp),
-                state = gridState,
+                modifier = Modifier.padding(horizontal = 6.dp),
+                state = gridState
             ) {
+                item(
+                    span = { GridItemSpan(2) }
+                ) {
+                    ListHeader(
+                        options = options,
+                        selectedIndex = selectedIndex,
+                        onOptionSelected = { selectedIndex = it },
+                        onSearchClicked = { showBottomSheet = true },
+                        modifier = Modifier.padding(horizontal = 10.dp, vertical = 8.dp)
+                    )
+                }
                 manga?.data?.forEach { manga ->
                     item {
                         MangaImageCard(manga) {
@@ -80,21 +106,98 @@ fun AdvancedSearchScreen(navController: NavController) {
                     }
                 }
             }
-        } else {
+        } else if (selectedIndex == 1) {
             LazyColumn(
-                modifier = Modifier.padding(start = 10.dp, end = 10.dp),
-                state = listState,
+                modifier = Modifier.padding(horizontal = 10.dp),
+                state = listState
             ) {
+                item {
+                    ListHeader(
+                        options = options,
+                        selectedIndex = selectedIndex,
+                        onOptionSelected = { selectedIndex = it },
+                        onSearchClicked = { showBottomSheet = true },
+                        modifier = Modifier.padding(horizontal = 10.dp, vertical = 8.dp)
+                    )
+                }
                 manga?.data?.forEach { manga ->
                     item {
-                        MangaTextCard(
-                            manga,
-                        ) {
+                        MangaTextCard(manga) {
                             navController.navigate("title/${manga.id}")
                         }
                     }
                 }
             }
+        }
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                onDismissRequest = { showBottomSheet = false },
+                sheetState = sheetState
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    
+                    TextField(
+                        value = searchText.value,
+                        onValueChange = { searchText.value = it },
+                        modifier = Modifier.fillMaxWidth(),
+                        singleLine = true,
+                        placeholder = { Text("Search") },
+                        label = { Text("Search") }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ListHeader(
+    options: List<String>,
+    selectedIndex: Int,
+    onOptionSelected: (Int) -> Unit,
+    onSearchClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SingleChoiceSegmentedButtonRow(
+            modifier = Modifier.padding(end = 10.dp)
+        ) {
+            options.forEachIndexed { index, label ->
+                SegmentedButton(
+                    selected = index == selectedIndex,
+                    onClick = { onOptionSelected(index) },
+                    shape = SegmentedButtonDefaults.itemShape(index, options.size),
+                    label = {
+                        Icon(
+                            painter = painterResource(
+                                when (label) {
+                                    "Cover Grid" -> if (selectedIndex == index) R.drawable.grid else R.drawable.grid_outline
+                                    "Detailed List" -> if (selectedIndex == index) R.drawable.list else R.drawable.list_outline
+                                    else -> R.drawable.information
+                                }
+                            ),
+                            contentDescription = stringResource(R.string.search),
+                            modifier = Modifier.size(20.dp)
+                        )
+                    }
+                )
+            }
+        }
+        Button(onClick = onSearchClicked) {
+            Icon(
+                painter = painterResource(R.drawable.search_outline),
+                contentDescription = stringResource(R.string.search),
+                modifier = Modifier.size(20.dp)
+            )
         }
     }
 }
